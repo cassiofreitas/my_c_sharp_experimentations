6a636456f92da7210895c03eb5942e7c0763a576
Cassio Freitas <cassiofreitas@cassiofreitas.com>
2022-06-24 18:55:46 -0300

    Add project files.

---
+++ pre_pos_incremento/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace pre_pos_incremento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
