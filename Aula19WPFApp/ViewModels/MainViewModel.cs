5ec7cd5eb7cf40849d47b7a3ae09d28180f68d86
Cassio Freitas <cassiofreitas@cassiofreitas.com>
2022-06-30 17:51:22 -0300

    Updating solution copied from class material (DHME)

---
+++ Aula19WPFApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace Aula19WPFApp.ViewModels
+{
+    public class MainViewModel: Screen
+    {
+        private string nomeCompleto;
+
+        public string NomeCompleto
+        {
+            get
+            {
+                return nomeCompleto;
+            }
+            set
+            {
+                nomeCompleto = value;
+                base.NotifyOfPropertyChange(() => NomeCompleto);
+            }
+        }
+    }
+}
